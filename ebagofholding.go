b6afc232fbf31ffbba27b3173b46a85242782dab
James <github@tasermonkeys.com>
2014-11-04 18:36:57 -0500

    Adding ability to choose how many workers to use

--- ebagofholding.go
+++ ebagofholding.go
@@ -9,3 +9,13 @@ func (goenv *Goenv) GetListBatchSize() (listBatchSize int) {
 	listBatchSize = goenv.GetIntFailToEnv("EBOH_LIST_BATCH_SIZE", 20)
 	return
 }
+
+func (goenv *Goenv) GetUploadWorkers() (listBatchSize int) {
+	listBatchSize = goenv.GetIntFailToEnv("EBOH_UPLOAD_WORKERS", 5)
+	return
+}
+
+func (goenv *Goenv) GetDownloadWorkers() (listBatchSize int) {
+	listBatchSize = goenv.GetIntFailToEnv("EBOH_DOWNLOAD_WORKERS", 5)
+	return
+}
